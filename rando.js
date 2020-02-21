1060cf0b1a07f75680d9c8fde5456eb82a1e73f3
kavirajesh22 <44529969+kavirajesh22@users.noreply.github.com>
2020-02-20 22:53:34 -0800

    Update rando.js

--- rando.js
+++ rando.js
@@ -11,19 +11,22 @@ $(document).ready(function(){
         var statusElm = $('.status')
         statusElm.empty()
         
-        if (email.includes('@')==false && email.includes('.')==false && email.length<5){
-             statusElm.append('<div>Your email is not valid</div>')
+        if ((email.includes('@')==false) || (email.includes('.')==false) || (email.length<5)){
+             statusElm.append('<div  style="margin-left: 50%; margin-top: 2%; color: red">Your email is not valid.</div>')
+            
         }
-    
-        if (name.includes('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0') && name.length<2){
-            statusElm.append('<div>Your name is not valid. </div>')
+   
+        if (name.length<3){
+            statusElm.append('<div  style="margin-left: 50%; margin-top: 0.3%; color: red">Your name is not valid.</div>')
         }
-        
+   
         if (name.includes('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')==false && name.length>1 && email.includes('@')==true && email.includes('.')==true && email.length>5){
-            statusElm.append('<div>Thanks for submitting! We will get back to you soon. </div>')
+            statusElm.append('<div style="margin-left: 50%; margin-top: 2%">Thanks for submitting! We will get back to you soon. </div>')
         }
      
     })
     
+    
+})
     
 })
